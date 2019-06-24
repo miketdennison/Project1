4a7db3a0017547ce33a7a25fa8f7822175c3f460
Bri <breezy.aguilar@gmail.com>
2019-06-24 12:40:11 -0500

    A little javascript has been added to the buttons for a card transition

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,16 @@
-$(document).ready {
+$(document).ready(function() {
+
+    $("#green-check").on("click", function () {
+        $(".card").toggle("slide", {direction: "right"}, 400);
+    });
 
 
 
+    $("#red-check").on("click", function () {
+        $(".card").toggle("slide", {direction: "left"}, 400);
+
+        $(".card").show();
+    })
+
+
+})
