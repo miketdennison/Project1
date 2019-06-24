dbfabf5e24a2dec34d7c16395dc365a129aed1d4
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-24 12:46:48 -0500

    changed from table to unordered list.  Made it show up in two different columns

--- assets/javascript/survey.js
+++ assets/javascript/survey.js
@@ -1,9 +1,21 @@
-$("#surveyQuestion").text("What type of food would you like to eat?")
+
 
 var food = ["American", "Italian", "Mexican", "Barbeque", "Breakfast & Brunch", "Chicken Wings"]
+// var checkbox = "assets/images/checkbox.png"
 
-var checkbox = "assets/images/checkbox.png"
+function type(){
+for(var i = 0; i < food.length; i++){
+    var check = $("<img>");
+    check.attr("src", "assets/images/checkbox.png")
+    check.attr("data-state",false);
+    check.attr("food", food[i])
+    check.addClass("check");
+    $("#surveyQuestion").text("What type of food would you like to eat?")
+    // $("#surveyAnswer").append("<li>"+check + " " + food[i]+"</li>")
+    $("#surveyAnswer").append("<li>")
+    $("#surveyAnswer").append(check);
+    $("#surveyAnswer").append(" "+food[i]+"</li>")
+}
+}
 
-for(var i = 0; i < food.length; i+=2){
-    $("#surveyAnswer").append("<tr>" + "<td>" + "<img src=" + checkbox + ">" + " " + food[i] + "</td>"  + "<td>" + "<img src=" + checkbox + ">" + " " + food[i+1] + "</td>" + "</tr>");
-    }
+type();
